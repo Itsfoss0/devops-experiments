49effee7d1a65d09c33b02f665d0416fa28ba059
Itsfoss0 <com01321@uoeld.ac.ke>
2023-12-25 18:43:28 +0300

    [Chore]: Initial Commit

---
+++ web/urls.py
@@ -0,0 +1,10 @@
+"""
+Base URLs
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls'))
+]
